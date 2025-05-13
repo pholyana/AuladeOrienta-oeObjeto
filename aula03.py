0372143578118798208b339b1109b4f7ccc1a621
pholyana nyele <pholyanarodrigues6@gmail.com>
2025-05-13 11:53:54 -0300

    novasatvdd

---
+++ aula03.py
@@ -0,0 +1,4 @@
+from biblioteca import Forma, Retangulo, Triangulo
+
+valor01=Retangulo(20)
+valor01.area()
--- biblioteca.py
+++ biblioteca.py
@@ -49,6 +49,132 @@ class conta_bancaria():
     def ativsarconta(self):
         if self.statusdaconta==False:
             self.statusdaConta=True
-            print(f"{self.nomeCliente} conta ativada")
         else:
             print(f"conta ja está ativada")
+
+
+
+
+
+
+class Animal():
+    def __init__(self,nome,cor):
+        self.nome=nome
+        self.cor=cor
+
+
+    def comer(self):
+        print(f"{self.nome} foi comer")
+
+
+class Gato(Animal):
+    def __init__(self, nome,cor):
+        super().__init__(nome,cor)
+
+    def miar(self):
+        print(f"o {self.nome} está miando ")
+
+
+class Vaca(Animal):
+    def __init__(self, nome ,cor):
+        super().__init__(nome,cor)
+
+    def rugir(self):
+        print(f"{self.nome} faz muuuuuhhh")
+
+
+class Cachorro(Animal):
+    def __init__(self, nome,cor):
+        super().__init__(nome,cor)
+
+    def latir(self):
+        print(f"{self.nome} está latindo")
+
+
+class Coelho(Animal):
+    def __init__(self,nome,cor):
+        super().__init__(nome,cor)
+
+    def grunindo(self):
+        print(f"{self.nome} está grunindo")
+
+
+class Ingresso():
+    def __init__(self,valor):
+        self.valor=valor
+        print(f"{self.valor} este é o valor do ingresso")
+
+    def imprimeValor(self):
+        print(f" o ingresso custa: {self.valor}")
+
+
+class VIP(Ingresso):
+    def __init__(self,valor):
+        super().__init__(valor)
+        self.valor2=valor*1.5
+
+    def imprimeValor(self):
+        print(f" O seu ingresso vip custa :{self.valor2}")
+
+
+class Forma():
+    def __init__(self):
+        self.area=0
+        self.perimetro=0
+
+
+class Retangulo(Forma):
+    def __init__(self):
+        super().__init__()
+
+
+    def calculaArea(self,base,altura):
+        self.area = base*altura
+        print(f"a area do retangulo é {self.area} :")
+
+    def calcularperimetro(self, base,altura):
+        self.perimetro=2*(base+altura)
+        print(f"o perimetro do retangulo é {self.perimetro}")
+
+class Triangulo(Forma):
+    def __init__(self,altura):
+        super().__init__()
+
+
+
+class Atleta():
+    def __init__(self):
+        self.aposentado=False
+        self.peso=0
+       
+
+    def aquecer(self):
+        print("ele está aquecendo")
+        if self.aposentado==True:
+            print("ele está aposentado e não pode aquecer")
+            return
+
+
+    def aposentar(self):
+            print("ele esta aquecendo")
+
+
+class Corredor(Atleta):
+    def __init__(self):
+        super().__init__()
+
+    def correr(self):
+        print(f"ele está corrrendo")
+
+
+
+class Nadador(Atleta):
+    def __init__(self):
+        super().__init__()
+        print(f"ele está nadando")
+
+
+
+class Ciclista(Atleta):
+    def __init__(self):
+        super().__init__()
